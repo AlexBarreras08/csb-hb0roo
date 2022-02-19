ae1c2669683fb4423de6a0543b6509336685a847
Code Sandbox <hello@codesandbox.io>
2022-02-19 20:59:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2ce0570d82f7a6e85d055c1fc49491ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f661cfa3cf06f7581795",
+    "url": "https://alexbarreras08.github.io/csb-hb0roo/static/css/main.d1eb86a8.chunk.css"
+  },
+  {
+    "revision": "f661cfa3cf06f7581795",
+    "url": "https://alexbarreras08.github.io/csb-hb0roo/static/js/main.eca499f4.chunk.js"
+  },
+  {
+    "revision": "ee308078c3cfcfc69e18",
+    "url": "https://alexbarreras08.github.io/csb-hb0roo/static/js/runtime~main.0bb22f7c.js"
+  },
+  {
+    "revision": "6776fbcce13746303bb1",
+    "url": "https://alexbarreras08.github.io/csb-hb0roo/static/js/2.573ff2ae.chunk.js"
+  },
+  {
+    "revision": "ade3d4dd50708cfde3559cc625b71b7c",
+    "url": "https://alexbarreras08.github.io/csb-hb0roo/index.html"
+  }
+];
